9469826760a0d99eab4f78c27806865e206cc0f0
liuyunxiaoPC <1614553756@qq.com>
2020-02-14 20:00:41 +0800

    修改json文件

--- demo.js
+++ demo.js
@@ -1,5 +1,8 @@
 let sum = require('./math').sum;
 let mul = require('./math').mul;
+let person = require('./data.json');
 
 console.log(sum(100, 200));
 console.log(mul(100, 200));
+
+console.log(person);
